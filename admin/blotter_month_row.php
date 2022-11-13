c8bd1e95e9c806321edba226757ac45ade8b2626
marArcz <mar19arcz@gmail.com>
2022-11-13 22:19:48 +0800

    updates

--- admin/blotter_month_row.php
+++ admin/blotter_month_row.php
@@ -1,17 +1,16 @@
 <?php
-        if(isset($_POST['year'])){
-        session_start();
-        require("./includes/conn.php");
-        $rows=array();
+if (isset($_POST['year'])) {
+    require("./includes/conn.php");
+    $rows = [];
 
-        $query = prep_stmt("SELECT * FROM blotter_history WHERE date LIKE ?");
-        $year = "%".$_POST['year']."%";
-        $query->bind_param('s',$year);
-        $query->execute();
-        $res = $query->get_result();
-        while($row = $res->fetch_assoc()){
-            array_push($rows, $row);
-        }
-        
-        echo json_encode($rows);
-        }
+    $query = prep_stmt("SELECT * FROM blotter_history WHERE date = ");
+    $year = $_POST['year'];
+    $query->bind_param('s', $year);
+    $query->execute();
+    $res = $query->get_result();
+    while ($row = $res->fetch_assoc()) {
+        array_push($rows, $row);
+    }
+
+    echo json_encode($rows);
+}
